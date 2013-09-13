be76d397f1f67eb74da737fb33b27c6bc3f53ffe
Brett Boston <bboston7@gmail.com>
2013-09-13 16:03:42 -0700

    added anagram.rkt

---
+++ anagram.rkt
@@ -0,0 +1,13 @@
+#lang racket
+
+#|
+Takes two strings and determines whether or not they are anagrams of eachother
+
+(anagram? s1 s2) -> bool?
+  s1 : string?
+  s2 : string?
+|#
+(define (anagram? s1 s2)
+  (define (fix-string str)
+    (sort (remove* (list #\space) (string->list s1)) char>?))
+  (equal? (fix-string s1) (fix-string s2)))
