bbc98d8e21a2b02de9418c33ad7318e6f21bfd78
Brett Boston <bboston7@gmail.com>
2013-09-13 17:34:36 -0700

    added unit tests for anagram?

---
+++ anagram-test.rkt
@@ -0,0 +1,12 @@
+#!/usr/bin/racket
+#lang racket/base
+
+(require rackunit
+         "anagram.rkt")
+
+(check-true (anagram? ("abc" "abc")) "Equal strings")
+(check-trye (anagram? ("" "")) "Empty strings")
+(check-true (anagram? ("acb" "abc")) "anagram strings")
+(check-true (anagram? ("abc" "cba")) "reversed strings")
+(check-true (anagram? ("a" "a")) "Single character")
+(check-false (anagram? ("abc" "acc")) "Not anagrams")
