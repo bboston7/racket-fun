bfc9b0d05250fde90c84f1dc6d4c4cf870b5904b
Brett Boston <bboston7@gmail.com>
2013-09-13 21:46:02 -0700

    wrote all-pass?

--- anagram-test.rkt
+++ anagram-test.rkt
@@ -4,11 +4,23 @@
 (require rackunit
          "anagram.rkt")
 
-(check-true (anagram? "abc" "abc") "Equal strings")
-(check-true (anagram? "" "") "Empty strings")
-(check-true (anagram? "acb" "abc") "anagram strings")
-(check-true (anagram? "abc" "cba") "reversed strings")
-(check-true (anagram? "a" "a") "Single character")
-(check-false (anagram? "abc" "acc") "Not anagrams")
-(check-false (anagram? "totally" "different") "totally different")
-(fail "blah bleh")
+(define-test-suite anagram-tests
+  (check-true (anagram? "abc" "abc") "Equal strings")
+  (check-true (anagram? "" "") "Empty strings")
+  (check-true (anagram? "acb" "abc") "anagram strings")
+  (check-true (anagram? "abc" "cba") "reversed strings")
+  (check-true (anagram? "a" "a") "Single character")
+  (check-false (anagram? "abc" "acc") "Not anagrams")
+  (check-false (anagram? "totally" "different") "totally different"))
+  ;(fail "blah bleh"))
+
+(define (all-pass? suite)
+  (fold-test-results
+    (lambda (result seed)
+      (if (not (test-success? result))
+        #f
+        seed))
+    #t
+    suite))
+
+(all-pass? anagram-tests)
