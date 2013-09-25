e04875a142c7f5847b1fca42b38be60a9a30d2a0
Brett Boston <bboston7@gmail.com>
2013-09-25 00:06:51 -0700

    Changed eval-quad to use a quad struct

--- quad-test.rkt
+++ quad-test.rkt
@@ -7,8 +7,8 @@
 
 (define-test-suite
   eval-quad-tests
-  (check-equal? (eval-quad 1 3 -4) '(1 . -4) "basic eval-quad test")
-  (let ([res (eval-quad 2 -4 -3)])
+  (check-equal? (eval-quad (quad 1 3 -4)) '(1 . -4) "basic eval-quad test")
+  (let ([res (eval-quad (quad 2 -4 -3))])
     (check-= (car res) 2.58 0.01 "irrational x1")
     (check-= (cdr res) -0.58 0.01 "irrational x2")))
 
--- quad.rkt
+++ quad.rkt
@@ -3,8 +3,13 @@
 
 (provide (all-defined-out))
 
-(define (eval-quad a b c)
-  (let* ([root (sqrt (- (sqr b) (* 4 a c)))]
+(struct quad (a b c) #:transparent)
+
+(define (eval-quad coefs)
+  (let* ([a (quad-a coefs)]
+         [b (quad-b coefs)]
+         [c (quad-c coefs)]
+         [root (sqrt (- (sqr b) (* 4 a c)))]
          [denom (* 2 a)]
          [num (lambda (pom) (pom (- b) root))])
     (cons (/ (num +) denom) (/ (num -) denom))))
